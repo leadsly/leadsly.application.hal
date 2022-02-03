1c8370194f691bdd74f3d56e26bc643e1015cf7d
Oskar Mikolajczyk <omikolaj1@gmail.com>
2022-02-03 13:58:09 -0500

    chore: fix failing build

--- Hal/ProblemDetailsDescriptions.cs
+++ Hal/ProblemDetailsDescriptions.cs
@@ -2,6 +2,10 @@
 {
     public static class ProblemDetailsDescriptions
     {
-        
+        public const string ExpiredAccessTokenIsInvalid = "Expired access token is invalid.";
+        public const string BadRequest = "Some of the provided input data is invalid.";
+        public const string ForbiddenDetail = "The server understood the request, but is refusing to authorize it.";
+        public const string ExternalJwtIsInvalid = "External provider jwt is invalid.";
+        public const string Unauthorized = "Authentication credentials are missing or invalid.";
     }
 }
