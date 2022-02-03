c26ebeba1b320756a424aedd8c14a831e33555d8
Oskar Mikolajczyk <omikolaj1@gmail.com>
2022-02-03 00:37:18 -0500

    refactor: update project

--- API/Configurations/ApiDefaults.cs
+++ API/Configurations/ApiDefaults.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
-using Leadsly.Shared.Api.Filters;
 
 namespace Api.Configurations
 {
