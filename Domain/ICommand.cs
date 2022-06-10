4ed46c5d17c1ecfefb7ab3fe3c5ad17dbc96cfcb
Oskar Mikolajczyk <omikolaj1@gmail.com>
2022-06-10 02:27:55 -0400

    refactor: add properties to ICommand

--- Domain/ICommand.cs
+++ Domain/ICommand.cs
@@ -1,4 +1,6 @@
 ﻿using Leadsly.Application.Model.Campaigns;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,8 @@ namespace Domain
 {
     public interface ICommand
     {
+        public IModel Channel { get; }
+        public BasicDeliverEventArgs EventArgs { get; }
         public string StartOfWorkDay { get; }
         public string EndOfWorkDay { get; }
         public string TimeZoneId { get; }
